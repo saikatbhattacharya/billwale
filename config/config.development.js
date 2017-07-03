3fead65dbb0a662df79fbca9f2984b6343e0390f
Saikat Bhattacharya <saikat.bhattacharya@nationstarmail.com>
2017-07-03 15:47:33 +0530

    db updated

--- config/config.development.js
+++ config/config.development.js
@@ -1,3 +1,5 @@
 module.exports = {
-    "mongodb":"mongodb://billappadmin:admin123@ds163060.mlab.com:63060/billapp"
+    "mongodb":"mongodb://billwale-db:lXQfKZkgWFVcggimtK4vqPuWCA9mnv4GIwxl2NGanoyHtPewHL11imayncvQcvE9u6C5mD9qwHuetEudXCFBSQ==@billwale-db.documents.azure.com:10255/?ssl=true"
 }
+
+// "mongodb://billappadmin:admin123@ds163060.mlab.com:63060/billapp"
