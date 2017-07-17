d4c0885abaee3b4ac4613381813b02eaefbfbfe2
Saikat Bhattacharya <saikat.bhattacharya@nationstarmail.com>
2017-07-17 13:02:01 +0530

    db address changed

--- config/config.development.js
+++ config/config.development.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "mongodb":"mongodb://billwale-db:lXQfKZkgWFVcggimtK4vqPuWCA9mnv4GIwxl2NGanoyHtPewHL11imayncvQcvE9u6C5mD9qwHuetEudXCFBSQ==@billwale-db.documents.azure.com:10255/?ssl=true"
+    "mongodb":"mongodb://billappadmin:admin123@ds163060.mlab.com:63060/billapp"
 }
 
 // "mongodb://billappadmin:admin123@ds163060.mlab.com:63060/billapp"
