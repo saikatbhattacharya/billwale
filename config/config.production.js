485533b8a4dfcfb6e5830c3795c1169c92f59c86
Saikat Bhattacharya <saikat.bhattacharya@nationstarmail.com>
2017-08-18 16:14:21 +0530

    DB connection moved to env var

--- config/config.production.js
+++ config/config.production.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "mongodb":"",//"mongodb://billappadmin:admin123@ds163060.mlab.com:63060/billapp",
+    "mongodb": process.env.DB_CONNECTION,//"mongodb://billappadmin:admin123@ds163060.mlab.com:63060/billapp",
     "translationURL": "https://translate.googleapis.com/translate_a/single"
 }
